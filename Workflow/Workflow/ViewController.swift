d7ce6b455cdc36aa3a0eef91afbb2fa331d40bae
Sam Fox <sof5207@psu.edu>
2018-08-24 18:36:51 -0400

    Added App Icons.

---
+++ Workflow/Workflow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workflow
+//
+//  Created by Samuel Fox on 8/24/18.
+//  Copyright © 2018 Samuel Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
