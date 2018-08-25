6c699a561a6a2eae101350475190b0bb211cacd2
Sam Fox <sof5207@psu.edu>
2018-08-25 14:49:24 -0400

    Created multiplication logic as well as the logic to build the spinner with random values and the correct result as well.

--- Multiplication Exercises/Multiplication Exercises/ViewController.swift
+++ Multiplication Exercises/Multiplication Exercises/ViewController.swift
@@ -18,23 +18,54 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet var submitNextButton: UIButton!
     @IBOutlet var correctLabel: UILabel!
     
-    let defaultAnswerChoices = [0, 0, 0, 0]
-    
+    var answerChoicesCollection = [0, 0, 0, 0]
+    var internalMultiplicand = 0
+    var internalMultiplier = 0
+    var internalResult = 0
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return defaultAnswerChoices.count
+        return answerChoicesCollection.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return String(defaultAnswerChoices[row])
+        return String(answerChoicesCollection[row])
+    }
+
+    func generateMultiplicationValues(){
+        internalMultiplicand = Int(arc4random_uniform(15)+1)
+        internalMultiplier = Int(arc4random_uniform(15)+1)
+        multiplicand.text = String(internalMultiplicand)
+        multiplier.text = String(internalMultiplier)
+        internalResult = internalMultiplicand * internalMultiplier
+        
+        var randomResult = 0
+        var valueToAdd = 0
+        var internalResultCollection = [internalResult]
+        
+        while internalResultCollection.count <= 4 {
+            randomResult = internalResult + Int(arc4random_uniform(5))+1
+            if !(internalResultCollection.contains(randomResult)) {
+                internalResultCollection.append(randomResult)
+            }
+        }
+        var count = 0
+        while answerChoicesCollection.contains(0) && count <= 4 {
+            valueToAdd = internalResultCollection[Int(arc4random_uniform(4))]
+            if !(answerChoicesCollection.contains(valueToAdd)){
+                answerChoicesCollection[count] = valueToAdd
+                count = count + 1
+            }
+        }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        generateMultiplicationValues()
         submitNextButton.setTitle("Submit", for: UIControlState.normal)
         answerChoices.delegate = self
         answerChoices.dataSource = self
@@ -46,6 +77,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
 
     @IBAction func submitAndNextButton(_ sender: Any) {
+        let selectedChoice = answerChoices.selectedRow(inComponent: 1)
+        
+        
         
     }
     
