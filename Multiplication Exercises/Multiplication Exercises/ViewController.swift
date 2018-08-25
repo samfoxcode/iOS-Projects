cec676de4081df2deb074c6633d52ab484431837
Sam Fox <sof5207@psu.edu>
2018-08-25 14:07:08 -0400

    Added action for Button as well as added UIPicker connection/dummy data.

--- Multiplication Exercises/Multiplication Exercises/ViewController.swift
+++ Multiplication Exercises/Multiplication Exercises/ViewController.swift
@@ -8,20 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet var multiplicand: UILabel!
     @IBOutlet var multiplier: UILabel!
     @IBOutlet var result: UILabel!
-    
     @IBOutlet var answerChoices: UIPickerView!
     @IBOutlet var questionsStatus: UILabel!
     @IBOutlet var submitNextButton: UIButton!
     @IBOutlet var correctLabel: UILabel!
     
+    let defaultAnswerChoices = [0, 0, 0, 0]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return defaultAnswerChoices.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(defaultAnswerChoices[row])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        submitNextButton.setTitle("Submit", for: UIControlState.normal)
+        answerChoices.delegate = self
+        answerChoices.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +45,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func submitAndNextButton(_ sender: Any) {
+        
+    }
+    
 }
 
