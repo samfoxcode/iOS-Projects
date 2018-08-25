83427729d736e2452f6700ee33aa728fbc6267d9
Sam Fox <sof5207@psu.edu>
2018-08-25 13:46:02 -0400

    Added outlets for all UI items.

--- Multiplication Exercises/Multiplication Exercises/ViewController.swift
+++ Multiplication Exercises/Multiplication Exercises/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var multiplicand: UILabel!
+    @IBOutlet var multiplier: UILabel!
+    @IBOutlet var result: UILabel!
+    
+    @IBOutlet var answerChoices: UIPickerView!
+    @IBOutlet var questionsStatus: UILabel!
+    @IBOutlet var submitNextButton: UIButton!
+    @IBOutlet var correctLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
