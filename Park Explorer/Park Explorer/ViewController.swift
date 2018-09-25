e07673efc8cfc801d486a3c4104d518f0135a68a
Sam Fox <sof5207@psu.edu>
2018-09-24 22:42:17 -0400

    Disable scrolling until image is scaled back to 1.0.

--- Park Explorer/Park Explorer/ViewController.swift
+++ Park Explorer/Park Explorer/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     let parkModel = Model()
     let numberOfParks : Int
     var parkPages = [UIView]()
-    var offset : CGFloat = 0.0
+    var xOffset : CGFloat = 0.0
+    var yOffset : CGFloat = 0.0
+    var pictureLimit = [Int:Int]()
+    var currentParkPage = 0
+    var disabled = false
+    
     @IBOutlet var mainScrollView: UIScrollView!
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,22 +30,6 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     func configureScrollView() {
         mainScrollView.isPagingEnabled = true
         mainScrollView.delegate = self
-        /*
-        print(numberOfParks)
-        for i in 0..<numberOfParks {
-            let view = UIScrollView(frame: CGRect.zero)
-            let title = UILabel(frame: CGRect.zero)
-            //view.minimumZoomScale = 0.1
-            //view.maximumZoomScale = 10
-            //view.delegate = self
-            title.text = parkModel.park(i).name
-            title.textAlignment = .center
-            view.addSubview(title)
-            parkPages.append(view)
-            mainScrollView.addSubview(view)
-        }
-        */
- 
     }
     
     override func viewDidLoad() {
@@ -55,14 +44,12 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         let size = self.view.bounds.size
         mainScrollView.contentSize = CGSize(width: size.width * CGFloat(parkModel.lengthOfParks()), height: size.height*7)
         var offset : CGFloat = 0.0
+        
         // lay out all the pages on the scroll view
         for i in 0..<parkModel.lengthOfParks() {
-            // set each page's fram
-            let point = CGPoint(x: CGFloat(i)*size.width, y: 0)
 
             // set title label's frame
-            
-            let label = UILabel(frame: CGRect.zero)//page.subviews[0] // only subview
+            let label = UILabel(frame: CGRect.zero)
             let labelHeight : CGFloat = 20.0
             let labelWidth : CGFloat = size.width
             label.frame = CGRect(x: CGFloat(i)*size.width, y: CGFloat(30), width: labelWidth, height: labelHeight)
@@ -70,10 +57,11 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             label.textAlignment = .center
             mainScrollView.addSubview(label)
             
+            pictureLimit[i] = parkModel.parkInfoLength(i)
+            
             for pictureIndex in 0..<parkModel.parkInfoLength(i) {
                 print("\(parkModel.parkName(i))0\(pictureIndex)")
                 let image = UIImage(named: "\(parkModel.parkName(i))0\(pictureIndex+1)")
-                //print(image)
                 let imageView = UIImageView(image: image)
                 
                 if pictureIndex == 0 {
@@ -83,30 +71,22 @@ class ViewController: UIViewController, UIScrollViewDelegate {
                     offset = 0.0
                 }
                 
+                
                 let scrollFrame = CGRect(x: CGFloat(i)*size.width, y: CGFloat(pictureIndex)*size.height+offset, width: size.width, height: size.height-offset)
                 let imageScrollView = UIScrollView(frame: scrollFrame)
                 imageScrollView.contentSize = CGSize(width: size.width, height: size.height)
                 imageScrollView.delegate = self
-                //imageView.center = imageScrollView.center
                 let imageHeightScale = size.width/(image?.size.width)!
-                //print(imageHeightScale)
                 let imageViewSize = CGSize(width: (image?.size.width)!*imageHeightScale, height: (image?.size.height)!*imageHeightScale)
                 imageView.frame.size = imageViewSize
-                //imageView.frame = imageViewFrame
-                //imageView.center.x = imageScrollView.center.x
-                //imageView.center.y = imageScrollView.center.y - offset
-                
-                //print(imageScrollView.subviews)
-                //print(imageScrollView)
-                imageScrollView.minimumZoomScale = 0.1
+                imageScrollView.minimumZoomScale = 1.0
                 imageScrollView.maximumZoomScale = 10.0
                 imageScrollView.tag = pictureIndex
                 imageScrollView.zoomScale = imageHeightScale
-                //imageScrollView.zoomScale  = scaleSize
+                
                 imageScrollView.addSubview(imageView)
                 imageView.center = CGPoint(x: imageScrollView.contentSize.width/2.0, y: (imageScrollView.contentSize.height-offset)/2.0)
                 mainScrollView.addSubview(imageScrollView)
-                //print(imageScrollView.subviews)
             }
             
         }
@@ -129,8 +109,6 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         if scrollView != mainScrollView && scrollView.subviews.count > 0 {
-            //print(scrollView)
-            //print(scrollView.subviews)
             return scrollView.subviews[0]
         }
         return nil
@@ -138,24 +116,42 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         // Save the offset of the current page before it changes, then set it in scrollViewDidScroll
-        offset = mainScrollView.contentOffset.x
+        xOffset = mainScrollView.contentOffset.x
+        yOffset = mainScrollView.contentOffset.y
+        currentParkPage = Int(xOffset/self.view.bounds.width)
+        print(currentParkPage)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print(mainScrollView.contentOffset.x)
-        print(scrollView)
+        print(mainScrollView.contentOffset.y)
+        print(self.view.bounds.height)
+        print(pictureLimit[currentParkPage]!)
+        if mainScrollView.contentOffset.y >= (CGFloat(pictureLimit[currentParkPage]!)*self.view.bounds.height){
+            print("HIT")
+            mainScrollView.contentOffset.y = yOffset
+        }
+
         if mainScrollView.contentOffset.y > 0 && scrollView == mainScrollView {
             mainScrollView.showsHorizontalScrollIndicator = false
-            mainScrollView.contentOffset.x = offset
+            mainScrollView.contentOffset.x = xOffset
         }
         else {
             mainScrollView.showsHorizontalScrollIndicator = true
         }
+        
     }
  
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         // Update imageView center
         if scrollView != mainScrollView && scrollView.subviews.count > 0 {
+            if scrollView.zoomScale != 1.0 {
+                disabled = true
+                mainScrollView.isScrollEnabled = false
+            }
+            else {
+                disabled = false
+                mainScrollView.isScrollEnabled = true
+            }
             scrollView.subviews[0].center = centerForImage(scrollView)
         }
     }
