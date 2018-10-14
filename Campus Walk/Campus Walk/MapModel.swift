77714d1cc1dcfaa4dd16afa83e2b38c732ccf232
Sam Fox <sof5207@psu.edu>
2018-10-14 15:31:44 -0400

    Can list and plot buildings.

--- Campus Walk/Campus Walk/MapModel.swift
+++ Campus Walk/Campus Walk/MapModel.swift
@@ -9,4 +9,60 @@
 import Foundation
 import MapKit
 
+struct Building : Codable {
+    let name : String
+    let opp_bldg_code : Int
+    let year_constructed : Int
+    let latitude : Double
+    let longitude : Double
+    let photo : String
+}
+
+typealias Buildings = [Building]
+
+class CampusModel {
+    
+    fileprivate let allBuildings : Buildings
+    
+    static let sharedInstance = CampusModel()
+    
+    fileprivate init() {
+        
+        
+        let mainBundle = Bundle.main
+        let solutionURL = mainBundle.url(forResource: "buildings", withExtension: "plist")
+        
+        
+        do {
+            let data = try Data(contentsOf: solutionURL!)
+            let decoder = PropertyListDecoder()
+            allBuildings = try decoder.decode(Buildings.self, from: data)
+        } catch {
+            print(error)
+            allBuildings = []
+        }
+        
+    }
+    
+    func numberOfBuildings() -> Int {
+        return allBuildings.count
+    }
+    func nameOfBuilding(_ index : Int) -> String {
+        return allBuildings[index].name
+    }
+    func photoNameBuilding(_ index : Int) -> String {
+        return allBuildings[index].photo
+    }
+    func buildingLocation(_ name : String) -> CLLocation? {
+        for i in 0..<allBuildings.count {
+            if allBuildings[i].name == name {
+                print("hit")
+                let latitude = allBuildings[i].latitude
+                let longitude = allBuildings[i].longitude
+                return CLLocation(latitude: latitude, longitude: longitude)
+            }
+        }
+        return nil
+    }
+}
 
--- Campus Walk/Campus Walk/ViewController.swift
+++ Campus Walk/Campus Walk/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class CampusBuilding : NSObject, MKAnnotation {
+    let coordinate: CLLocationCoordinate2D
+    let title : String?
+    
+    init(title:String?, coordinate:CLLocationCoordinate2D) {
+        self.title = title
+        self.coordinate = coordinate
+    }
+}
 
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, PlotBuildingDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    let mapModel = CampusModel.sharedInstance
+    let locationManager = CLLocationManager()
+    let kSpanLatitudeDelta = 0.027
+    let kSpanLongitudeDelta = 0.027
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let coordinate  = CLLocationCoordinate2D(latitude: 40.8012, longitude: -77.859909)
+        let span = MKCoordinateSpan(latitudeDelta: kSpanLatitudeDelta, longitudeDelta: kSpanLongitudeDelta)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        locationManager.delegate = self
+    }
+    
+    func annotationView(forCampusBuilding campusBuilding:CampusBuilding) -> MKAnnotationView {
+        let identifier = "BuildingPin"
+        var view: MKPinAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+            view = dequeuedView
+        } else {
+            view = MKPinAnnotationView(annotation: campusBuilding, reuseIdentifier: identifier)
+            view.pinTintColor = MKPinAnnotationView.redPinColor()
+            view.animatesDrop = true
+            view.canShowCallout = true
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        
+        return view
+    }
+    
+    func plot(building: String) {
+        let coordinate = mapModel.buildingLocation(building)?.coordinate
+        let title = building
+        let campusBuilding = CampusBuilding(title: title, coordinate: coordinate!)
+        
+        UIView.animate(withDuration: 1, delay: 0.1, options: UIView.AnimationOptions.curveEaseOut, animations: {
+        let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+        let region = MKCoordinateRegion(center: coordinate!, span: span)
+            self.mapView.setRegion(region, animated: true)
+        self.mapView.addAnnotation(campusBuilding)
+        })
     }
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "BuildingList":
+            let navController = segue.destination as! UINavigationController
+            let buildingListViewController = navController.topViewController as! TableViewController
+            buildingListViewController.delegate = self
+            
+        default:
+            assert(false, "Unhandled Segue")
+        }
+    }
 
 }
 
