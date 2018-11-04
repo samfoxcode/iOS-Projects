b10cf2499c6f202d5e50e611ad7a6aa92e48e166
Sam Fox <sof5207@psu.edu>
2018-11-03 22:38:28 -0400

    Setting up Model.

--- Roam/Roam/Model.swift
+++ Roam/Roam/Model.swift
@@ -9,6 +9,55 @@
 import Foundation
 import Firebase
 
-class Model {
+enum PostAttributes: String {
+    case addedByUser
+    case description
+    case imagePath
+    case flights
+    case itinerary
+    case stays
+    case isPublic
+}
+
+struct Post {
+    let addedByUser: String
+    let description: String
+    let imagePath: String
+    let flights: String
+    let itinerary: String
+    let stays: String
+    let isPublic: Bool
+
+    init(addedByUser: String, description: String, imagePath: String, flights: String, itinerary: String, stays: String, isPublic: Bool){
+        self.addedByUser = addedByUser
+        self.description = description
+        self.imagePath = imagePath
+        self.flights = flights
+        self.itinerary = itinerary
+        self.stays = stays
+        self.isPublic = isPublic
+    }
     
+    init(snapshot: DataSnapshot) {
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        self.addedByUser = snapshotValue[PostAttributes.addedByUser.rawValue] as! String
+        self.description = snapshotValue[PostAttributes.description.rawValue] as! String
+        self.imagePath = snapshotValue[PostAttributes.imagePath.rawValue] as! String
+        self.flights = snapshotValue[PostAttributes.flights.rawValue] as! String
+        self.itinerary = snapshotValue[PostAttributes.itinerary.rawValue] as! String
+        self.stays = snapshotValue[PostAttributes.stays.rawValue] as! String
+        self.isPublic = snapshotValue[PostAttributes.isPublic.rawValue] as! Bool
+    }
+    
+    func toObject() -> Any {
+        return [
+            PostAttributes.addedByUser.rawValue:addedByUser,
+            PostAttributes.description.rawValue:description,
+            PostAttributes.imagePath.rawValue:imagePath,
+            PostAttributes.flights.rawValue:flights,
+            PostAttributes.itinerary.rawValue:itinerary,
+            PostAttributes.stays.rawValue:stays,
+            PostAttributes.isPublic.rawValue:isPublic,
+        ]
+    }
 }
