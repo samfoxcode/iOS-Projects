c22ce296fe4e29566cfcf2c07e8ff46fbfd37fa8
Sam Fox <sof5207@psu.edu>
2018-11-20 14:20:52 -0500

    Can view posts of users that current user is following.

---
+++ Roam/Roam/HomeTableViewController.swift
@@ -0,0 +1,169 @@
+//
+//  HomeTableViewController.swift
+//  
+//
+//  Created by Samuel Fox on 11/20/18.
+//
+
+import UIKit
+import Firebase
+
+class HomeTableViewController: UITableViewController {
+
+    fileprivate var ref : DatabaseReference!
+    fileprivate var storageRef : StorageReference!
+    
+    let cachedImage = CachedImages()
+    
+    var posts = [Post]()
+    var cachedPosts = [Post]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        ref = Database.database().reference()
+        storageRef = Storage.storage().reference()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.refreshControl?.attributedTitle = NSAttributedString(string: "Let's GOOOOOO!!!!!")
+        var following = [String]()
+    ref.child(FirebaseFields.Users.rawValue).child("USERNAME").child("following").observe(.value) { (snapshot) in
+            for user in snapshot.children {
+                let temp = user as! DataSnapshot
+                print(temp.key)
+                print(temp.value!)
+                following.append(temp.key)
+            }
+        }
+        
+        ref.child(FirebaseFields.Posts.rawValue).observe(.value) { (snapshot) in
+            var posts = [Post]()
+            for postSnapshot in snapshot.children {
+                let post = Post(snapshot: postSnapshot as! DataSnapshot)
+                if following.contains(post.username) {
+                    posts.append(post)
+                }
+            }
+            self.posts = posts
+            let block = {
+                self.cachedPosts = self.posts
+                self.tableView.reloadData()
+                self.refreshControl?.endRefreshing()
+            }
+            DispatchQueue.main.async(execute: block)
+        }
+        super.viewWillAppear(animated)
+    }
+    
+    func downloadImage(_ indexPath: IndexPath, _ imageURL: String) {
+        let storage = storageRef.storage.reference(forURL: cachedPosts[indexPath.section].imagePath)
+        storage.getData(maxSize: 1*1024*1024) { (data, error) in
+            if error == nil {
+                //self.cachedPosts[indexPath.section].cachedImage = UIImage(data: data!)
+                let image = UIImage(data: data!)
+                self.cachedImage.cacheImage(imageURL, image!)
+            }
+            else {
+                print("Error:\(error ?? "" as! Error)")
+            }
+        }
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return self.cachedPosts.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section > 0 {
+            return 20.0
+        }
+        else {
+            return 10.0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect.zero)
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostTableViewCell
+        
+        downloadImage(indexPath, cachedPosts[indexPath.section].imagePath)
+        cell.globalPostImageView.image = cachedImage.getCachedImage(cachedPosts[indexPath.section].imagePath)
+        cell.post = self.cachedPosts[indexPath.section]
+        cell.globalPostExperienceDetails.tag = indexPath.section
+        return cell
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
